1ced03586d921a3e07e29e9452a2e240c826817a
James Chuang <jameschuang13@gmail.com>
2013-12-09 23:28:15 -0800

    change view for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,45 @@
 class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to posts_path
+		end
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update_attributes(post_params)
+			redirect_to posts_path
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
 end
