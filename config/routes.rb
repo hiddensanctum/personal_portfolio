5b787a35e576263899733d5c0fc1c6517ea1535a
James Chuang <jameschuang13@gmail.com>
2013-12-10 21:18:19 -0800

    change views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 NewPortfolio::Application.routes.draw do
   devise_for :users
   resources :posts
+  root :to => "welcome#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
