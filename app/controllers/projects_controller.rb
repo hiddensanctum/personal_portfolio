fac4da61b83c9433d08096c6642d568518f86136
James Chuang <jameschuang13@gmail.com>
2013-12-16 00:20:10 -0800

    add pagination to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
 	before_filter :authenticate_user!, except: [:index, :show]
 
   def index
-    @projects = Project.all
+    @projects = Project.paginate(:page => params[:page], :per_page => 5)
   end
 
   def new
