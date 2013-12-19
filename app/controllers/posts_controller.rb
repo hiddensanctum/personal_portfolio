4cabeb49923f33c0adb42361be9d578bd6666fab
James Chuang <jameschuang13@gmail.com>
2013-12-18 18:30:05 -0800

    sort posts by when they were created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,9 @@ class PostsController < ApplicationController
 
 	def index
 		if params[:tag]
-    	@posts = policy_scope(Post).tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 3)
+    	@posts = policy_scope(Post).order('created_at DESC').tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 3)
   	else
-    	@posts = policy_scope(Post).paginate(:page => params[:page], :per_page => 3)
+    	@posts = policy_scope(Post).order('created_at DESC').paginate(:page => params[:page], :per_page => 3)
   	end
 	end
 
