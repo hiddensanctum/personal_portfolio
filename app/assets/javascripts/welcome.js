679197a78531a1fd5190335cde86434c3bf6174a
James Chuang <jameschuang13@gmail.com>
2014-04-01 12:27:37 -0700

    attempt to fix responsiveness

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,4 +4,27 @@ $(document).ready(function() {
       $(this).contents().unwrap();
     }
   });
+  var maxHeight = -1;
+
+  $('.columns').each(function() {
+   if (maxHeight < $(this).height()){
+    maxHeight = $(this).height();
+   };
+   console.log($(this).height());
+   console.log(maxHeight);
+  });
+
+  $('.columns').each(function() {
+   $(this).height(maxHeight);
+  });
+
+  // window.onresize = function(event) {
+  //   $('.sidebar').each(function() {
+  //    maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+  //   });
+
+  //   $('.sidebar').each(function() {
+  //    $(this).height(maxHeight);
+  //   });
+  // };
 });
